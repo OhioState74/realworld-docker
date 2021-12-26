0db959534c4e2ba1493be2a6751042db1a26fd85
Grimkassad <andrii.ianko@deliverect.com>
2021-12-26 13:10:52 +0200

    add env var

--- api/start.py
+++ api/start.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!@!2222"
+    a = os.environ.get('a12', 'codeorion')
+    return f"Hello World!@!2222 {a}"
 
 
 if __name__ == "__main__":
